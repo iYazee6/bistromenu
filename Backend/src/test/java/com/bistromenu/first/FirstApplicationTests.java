e54ff029501d630804ec0277cbb964e3b9442dee
Yazeed <iyazee6@gmail.com>
2025-02-24 21:15:51 +0300

    Initial Commit

---
+++ Backend/src/test/java/com/bistromenu/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bistromenu.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ DBScripts/Create appuser with permission.sql
@@ -0,0 +1,6 @@
+
+
+
+create user appuser with password 'P@ssw0rd';
+
+grant all privileges on database bistromenu to appuser;
