c58213cc7ec130869a1641cb3bec5614fb603e72
Yazeed <iyazee6@gmail.com>
2025-03-06 00:15:43 +0300

    FrontEnd: Grouping cards by menu item, adding dark theme customizations, card click not button click,

--- FrontEnd/bistro-menu-app/src/pages/Home.jsx
+++ FrontEnd/bistro-menu-app/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from 'react';
-import { Form, Button, Container, Modal } from 'react-bootstrap';
+import { Form, Button, Container, Modal, Collapse } from 'react-bootstrap';
 import api from "../utils/api";
 import { FaCoffee, FaInfoCircle, FaPlus, FaMinus, FaShoppingCart } from "react-icons/fa";
 import { GiCroissant } from "react-icons/gi";
@@ -110,7 +110,7 @@ function Home() {
     const totalPrice = Object.entries(cart).reduce((acc, [itemId, quantity]) => {
         const item = menuItems.find((menuItem) => menuItem.id === parseInt(itemId)); // Find item by ID
         return acc + (item ? item.price * quantity : 0); // Multiply price by quantity
-      }, 0);
+    }, 0);
 
     const addToCart = (item) => {
         setCart((prevCart) => ({
@@ -130,6 +130,13 @@ function Home() {
         });
     };
 
+    const groupedItems = Object.entries(
+        filteredItems.reduce((acc, item) => {
+            acc[item.category] = acc[item.category] || [];
+            acc[item.category].push(item);
+            return acc;
+        }, {})
+    );
 
 
     return (
@@ -137,11 +144,11 @@ function Home() {
             <Form>
                 <div className='row border search-filter'>
                     <div className='col-6'>
-                        <input type="text" className="form-control me-2 boarderless-form" placeholder="Search ...." value={searchQuery} onChange={handleSearch} />
+                        <input type="text" className="form-control me-2 boarderless-form" placeholder="Search in [ title or description ]" value={searchQuery} onChange={handleSearch} />
                     </div>
                     <div className='col-6 d-flex px-0'>
                         <Form.Group controlId="filter" className="col-10">
-                            <Form.Control type="Filter" placeholder="Filter" className='form-control me-2 boarderless-form' />
+                            <Form.Control type="text" placeholder="Filter" className='form-control me-2 boarderless-form' />
                         </Form.Group>
                         <Button variant="outline-secondary" className="col-2 disabled" type="submit">
                             search
@@ -150,11 +157,10 @@ function Home() {
                 </div>
             </Form>
 
-            <div className='row my-2'>
+            <div className='row mt-4'>
                 {categories.map((category) => (
-                    <div className='col'>
+                    <div key={category.name} className={`col col-${category.name.replace(/\s+/g, '-')}`}>
                         <button
-                            key={category.name}
                             className={`btn btn-outline-secondary category-btn border-0 me-2 mb-2 ${selectedCategory === category.name ? "selected" : ""}`}
                             onClick={() => handleCategoryClick(category.name)} >
                             <div className="icon-container">
@@ -168,100 +174,98 @@ function Home() {
                 ))}
 
                 {cartCount > 0 && (
-                    <div className='col border-start'>
-                        <button className="btn btn-outline-secondary category-btn border-0 h-100 disabled">
-                        <div className="icon-container"><FaShoppingCart /></div> Cart ({cartCount}) 
-                            ({totalPrice} SR)
-                        </button>
-                    </div>
+                    <button className="btn btn-outline-secondary category-btn border-0 h-100 border-start disabled">
+                        <div className="icon-container"><FaShoppingCart /></div> Cart ({cartCount})
+                        ({totalPrice} SR)
+                    </button>
                 )}
             </div>
-
 
             <div className='row'>
                 {loading && <p>Loading ....</p>}
                 {error && <p className="text-danger">{error}</p>}
 
-                <div className="row">
-                    {filteredItems.length > 0 ? (
-                        filteredItems.map((item) => (
-                            <div key={item.id} className="col-4 mb-4">
-                                <div className="card shadow-sm">
-                                    <div className='row g-0'>
-                                        <div className='col-md-4 h-100'>
-                                            <img src={item.image} className='img-fluid rounded' alt='' />
-                                        </div>
-                                        <div className='col-md-8'>
-                                            <div className="card-body pt-0">
-                                                <h5 className="card-title m-0">{item.itemName} <FaInfoCircle className="info-icon" onClick={() => handleShowModal(item)} /></h5>
-
-                                                <p className="h6 text-muted m-0">{item.calorie} calorie</p>
-                                                {/* <p className="card-text">{item.description}</p> */}
-                                                <p className="fw-bold m-0">{item.price} SR</p>
-                                                {/* <button className="btn btn-primary">Order Now</button> */}
+                {Object.entries(
+                    filteredItems.reduce((acc, item) => {
+                        acc[item.category] = acc[item.category] || [];
+                        acc[item.category].push(item);
+                        return acc;
+                    }, {})
+                ).map(([category, items]) => (
+                    <div key={category} className="mb-4">
+                        <hr />
+                        <div>
+                            <h3 className="text-theme">{category}</h3>
+                            {/* <button className="btn btn-primary btn-inline" role='button' type="button" data-bs-toggle="collapse" data-bs-target={`#col-${category.replace(/\s+/g, '-')}`}>Collapse {category}</button> */}
+                        </div>
+                        <div id={`col-${category.replace(/\s+/g, '-')}`} className="row">
+                            {items.map((item) => (
+                                <div key={item.id} className="col-4 mb-4">
+                                    <div className="card shadow-sm p-3" onClick={() => handleShowModal(item)} >
+                                        <div className='row g-0'>
+                                            <div className='col-md-4 h-100'>
+                                                <img src={item.image} className='img-fluid rounded' alt={item.itemName} />
                                             </div>
-                                            <div className="d-flex align-items-center">
-                                                <button className="btn btn-danger bg-theme border-0 btn-sm me-2" onClick={() => removeFromCart(item)}>
-                                                    <FaMinus />
-                                                </button>
-                                                <span className="cart-quantity">{cart[item.id] || 0}</span>
-                                                <button className="btn btn-success bg-theme border-0 btn-sm ms-2" onClick={() => addToCart(item)}>
-                                                    <FaPlus />
-                                                </button>
+                                            <div className='col-md-8'>
+                                                <div className="card-body pt-0">
+                                                    <h5 className="card-title m-0">{item.itemName}
+                                                        {/* <FaInfoCircle className="info-icon mx-2"  onClick={() => handleShowModal(item)} /> */}
+                                                    </h5>
+                                                    <p className="h6 text-muted m-0">{item.calorie} calorie</p>
+                                                </div>
+                                                <div className="d-flex align-items-center justify-content-between w-100">
+                                                    <span className="fw-bold my-0 mx-2">{item.price} SR</span>
+                                                    <div className='align-self-end'>
+                                                        <button className="btn btn-danger bg-theme border-0 btn-sm me-2 btn-rfcart" onClick={() => removeFromCart(item)}>
+                                                            <FaMinus />
+                                                        </button>
+                                                        <span className="cart-quantity">{cart[item.id] || 0}</span>
+                                                        <button className="btn btn-success bg-theme border-0 btn-sm ms-2 btn-atcart" onClick={() => addToCart(item)}>
+                                                            <FaPlus />
+                                                        </button>
+                                                    </div>
+                                                </div>
                                             </div>
-
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
-                    ) : (
-                        !loading && <p>No menu item found.</p>
-                    )}
+                            ))}
+                        </div>
+                    </div>
+                ))}
 
+                {selectedItem && (
                     <Modal show={showModal} onHide={handleCloseModal} centered>
-                        {selectedItem && (
-                            <>
-                                <Modal.Header closeButton className='border-bottom-0'>
-                                    <Modal.Title>
-                                        {selectedItem.itemName}
-                                        <p className="text-muted h6 mb-0">{selectedItem.calorie} calorie</p>
-                                    </Modal.Title>
-                                </Modal.Header>
-                                <Modal.Body>
-                                    <p className='text-muted'>{selectedItem.description}</p>
-                                    <img src={selectedItem.image || "https://via.placeholder.com/400"} alt={selectedItem.name} className="img-fluid rounded mb-3" />
-                                </Modal.Body>
-                                <Modal.Footer>
-                                    {/* <Button variant="secondary" onClick={handleCloseModal}>
-                                        Close
-                                    </Button> */}
-                                    <span className='text-theme-color'>
-                                        {selectedItem.price} SR
-                                    </span>
+                        <Modal.Header closeButton className='border-bottom-0'>
+                            <Modal.Title>
+                                {selectedItem.itemName}
+                                <p className="text-muted h6 mb-0">{selectedItem.calorie} calorie</p>
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <p className='text-muted'>{selectedItem.description}</p>
+                            <img src={selectedItem.image || "https://via.placeholder.com/400"} alt={selectedItem.name} className="img-fluid rounded mb-3" />
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <span className='text-theme-color'>{selectedItem.price} SR</span>
 
-                                    <div className="d-flex align-items-center">
-                                        <button className="btn btn-danger bg-theme btn-sm me-2" onClick={() => removeFromCart(selectedItem)}>
-                                            <FaMinus />
-                                        </button>
-                                        <span className="cart-quantity">{cart[selectedItem.id] || 0}</span>
-                                        <button className="btn btn-success bg-theme btn-sm ms-2" onClick={() => addToCart(selectedItem)}>
-                                            <FaPlus />
-                                        </button>
-                                    </div>
-
-                                </Modal.Footer>
-                            </>
-                        )}
+                            <div className="d-flex align-items-center">
+                                <button className="btn btn-danger bg-theme border-0 btn-sm me-2 btn-rfcart" onClick={() => removeFromCart(selectedItem)}>
+                                    <FaMinus />
+                                </button>
+                                <span className="cart-quantity">{cart[selectedItem.id] || 0}</span>
+                                <button className="btn btn-success bg-theme border-0 btn-sm ms-2 btn-atcart" onClick={() => addToCart(selectedItem)}>
+                                    <FaPlus />
+                                </button>
+                            </div>
+                        </Modal.Footer>
                     </Modal>
-
-                </div>
-
+                )}
             </div>
-
-
         </Container>
     );
+
+
 }
 
 export default Home;
