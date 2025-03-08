7b7dc3e7555296d8574658250e6646c6e7cd0266
Yazeed <iyazee6@gmail.com>
2025-03-08 04:37:32 +0300

    Updating DB Scripts, .gitignore, commands

---
+++ DBScripts/scripts.sql
@@ -0,0 +1,20 @@
+
+
+
+-- CREATE TABLE users
+-- {
+--     id  int PRIMARY KEY,
+--     Created timestamp   NOT NULL   default(now()),
+--     Username
+--     Password
+--     Role
+
+--     FirstName   varchar(120)
+--     LastName    varchar(240)
+--     Email       
+--     Mobile
+--     Active
+--     LastLogin
+--     LastInfoUpdate
+
+-- }
